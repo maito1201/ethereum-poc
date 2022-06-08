60b46410c8d76c840c5d99a5b338b28e5c82bc44
maito1201 <itomasa0915@gmail.com>
2022-06-08 15:59:37 +0900

    refactoring

--- cli/main.go
+++ cli/main.go
@@ -23,23 +23,22 @@ const pkey = "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
 // 1 is agree, 2 is disagree
 var voteNum = big.NewInt(2)
 
-func main() {
-	// initialize
+var instance *poll.Poll
+var fromAddress common.Address
+var auth *bind.TransactOpts
+
+func init() {
 	client, err := ethclient.Dial(url)
 	if err != nil {
 		panic(err)
 	}
 
 	address := common.HexToAddress(addr)
-	instance, err := poll.NewPoll(address, client)
+	instance, err = poll.NewPoll(address, client)
 	if err != nil {
 		panic(err)
 	}
 
-	gasPrice, err := client.SuggestGasPrice(context.Background())
-	if err != nil {
-		panic(err)
-	}
 	privateKey, err := crypto.HexToECDSA(pkey)
 	if err != nil {
 		panic(err)
@@ -49,25 +48,34 @@ func main() {
 	if !ok {
 		panic("error casting public key to ECDSA")
 	}
-	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
-	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
-	if err != nil {
-		panic(err)
-	}
+	fromAddress = crypto.PubkeyToAddress(*publicKeyECDSA)
 
 	chainID, err := client.ChainID(context.Background())
 	if err != nil {
 		panic(err)
 	}
-	auth, err := bind.NewKeyedTransactorWithChainID(privateKey, chainID)
+
+	auth, err = bind.NewKeyedTransactorWithChainID(privateKey, chainID)
+	if err != nil {
+		panic(err)
+	}
+	auth.Value = big.NewInt(0)     // in wei
+	auth.GasLimit = uint64(300000) // in units
+
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
 	if err != nil {
 		panic(err)
 	}
 	auth.Nonce = big.NewInt(int64(nonce))
-	auth.Value = big.NewInt(0)     // in wei
-	auth.GasLimit = uint64(300000) // in units
-	auth.GasPrice = gasPrice
 
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	auth.GasPrice = gasPrice
+}
+
+func main() {
 	// execute contract
 	title, err := instance.GetPoll(nil)
 	if err != nil {
@@ -76,11 +84,12 @@ func main() {
 	fmt.Printf("Get Poll: %v\n", title)
 
 	// check vote result
-	v, err := instance.Votes(nil, fromAddress)
+	opts := bind.CallOpts{From: fromAddress, Context: context.Background()}
+	v, err := instance.CheckPoll(&opts)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Vote Result %v\n", v)
+	fmt.Printf("Check Poll: %v\n", v)
 
 	// vote if result is 0
 	if v.Cmp(big.NewInt(0)) == 0 {
@@ -88,11 +97,13 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		fmt.Printf("Vote %v\n", tx)
-		v, err = instance.Votes(nil, fromAddress)
+		fmt.Printf("Vote %+v\n", tx)
+
+		// recheck vote result
+		v, err := instance.CheckPoll(&opts)
 		if err != nil {
 			panic(err)
 		}
-		fmt.Printf("Votes %v\n", v)
+		fmt.Printf("Check Poll: %v\n", v)
 	}
 }
